56ddb8312859a4c9c9bb0438fd5dd1b3ff0d5728
E. N. Slaughter <enslaugh@gmail.com>
2021-06-02 21:05:18 -0400

    First version fully completed

--- form.js
+++ form.js
@@ -1,3 +1,20 @@
-function createSubmissionText() {
-  return null;
+function submitForm(event) {
+  event.preventDefault();
+  createSubmissionText();
 }
+
+function createSubmissionText() {
+  signupElements.innerHTML = "";
+  let heading = document.createElement("P");
+  heading.innerHTML = "Thanks for registering!";
+  let messageText = document.createElement("P");
+  messageText.innerHTML =
+    "You’ll receive a confirmation message at the address provided. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Feel free to opt out at any time.";
+  signupElements.appendChild(heading);
+  signupElements.appendChild(messageText);
+}
+
+let signupElements = document.querySelector(".signup-elements");
+let signupButton = document.getElementById("signup-button");
+
+signupButton.addEventListener("click", submitForm, false);
